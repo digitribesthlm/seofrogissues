99234e50c9cb2f45d8a5c4aedd76c76223df437d
digitribesthlm <patrik@makeablesthlm.se>
2025-01-13 13:01:38 +0100

    next

--- components/DashboardLayout.js
+++ components/DashboardLayout.js
@@ -29,7 +29,7 @@ export default function DashboardLayout({ children }) {
               </div>
               <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                 <Link
-                  href="/"
+                  href="/mongo"
                   className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                 >
                   Dashboard
